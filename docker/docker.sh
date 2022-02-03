5da754fcbd4f5d0978a9f45c5074af755fd1c8ce
Mark Beamer Jr <markbeamerjr@gmail.com>
2022-02-03 14:12:25 -0500

    Move dispendium to new docker hub organization

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build --tag lbry/dispendium:$TRAVIS_BRANCH ./
+docker build --tag odyseeteam/dispendium:$TRAVIS_BRANCH ./
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-docker push lbry/dispendium:$TRAVIS_BRANCH
+docker push odyseeteam/dispendium:$TRAVIS_BRANCH
